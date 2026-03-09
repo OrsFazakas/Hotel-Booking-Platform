85c23ad18da88a6cd343db99f0bb902ae6b5bd78
ofazakas <ricsi6372@gmail.com>
2026-03-09 14:27:49 +0200

    base dependecies

---
+++ src/main/java/edu/ntt/hotelbookingplatform/HotelBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.ntt.hotelbookingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingPlatformApplication.class, args);
+    }
+
+}
